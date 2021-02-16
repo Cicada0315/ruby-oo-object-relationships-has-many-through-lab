b9269a84e84cbbef46b19a9fa81b9412bf7c137d
jean <jin.ahn.1027@gmail.com>
2021-02-16 13:44:56 -0500

    commit tree e9c201d2d9716ea1f7d2025f754eead54bb4dc5d

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,14 @@
+class Song
+    @@all=[]
+    attr_accessor :artist, :genre, :title
+    def initialize(title, artist, genre)
+        @title=title
+        @artist=artist
+        @genre=genre
+        @@all << self
+    end
 
+    def self.all
+        @@all
+    end
+end
